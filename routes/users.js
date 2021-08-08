3b4831df7409d53ceb4f81c934afe7847fc46644
Jubaer Jami <jamijubaer@gmail.com>
2021-08-08 16:39:20 +0600

    configured jwt

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
+
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken")
 
 const express = require('express');
 const router = express.Router();
+
+
 
 //importing models
 const User = require('../models/user');
@@ -12,7 +17,10 @@ router.post('/registration', async (req, res) => {
     try {
         const user = new User(req.body);
         await user.save();
-        res.status(201).send("Success");
+        user_data = { user_id: user.id }
+        const accessToken = jwt.sign(user_data, process.env.ACCESS_TOKEN_SECRET);
+
+        res.status(201).json({ accessToken: accessToken });
     } catch (error) {
         console.log(error)
         res.status(500).send("Failed");
@@ -22,20 +30,24 @@ router.post('/registration', async (req, res) => {
 router.post('/login', async (req, res) => {
     User.findOne({ username: req.body.username })
         .then(async result => {
-          
-            if(result==null){
+
+            if (result == null) {
                 return res.status(400).send('Cannot Find User');
             }
-            try{
-             
-                if(await bcrypt.compare(req.body.password,result.password)){
-                    return res.status(202).send("success");
-                }else{
+            try {
+
+                if (await bcrypt.compare(req.body.password, result.password)) {
+                    const user = { user_id: result.id };
+                    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+
+                    return res.status(202).json({ accessToken: accessToken });
+                } else {
                     return res.status(400).send('failed');
                 }
-               
 
-            }catch(error){
+
+            } catch (error) {
                 res.status(500).send()
             }
         })
